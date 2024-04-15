4c545d8b6743d8e941dbcfa544eac23e7275b815
KOIMN <v.bermudo.unity@gmail.com>
2024-04-15 20:46:47 +0200

    15/04 Fin Clase

--- Assets/_GameAssets/Scripts/GameManager.cs
+++ Assets/_GameAssets/Scripts/GameManager.cs
@@ -1,8 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
-using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
@@ -10,19 +11,42 @@ public class GameManager : MonoBehaviour
     //Atributos del juego
     public float salud;
     public int saludMaxima;
-    public int puntuacion;
+    public int puntuacion=0;
+    public int puntuacionMaxima=0;
 
     //ESTO SE PUEDE HACER CON EVENTOS
     public Image imageVida;
+    public TextMeshProUGUI textoPuntuacion;
+    public TextMeshProUGUI textoPuntuacionMaxima;
+
+    public List<GameObject> objetosAActivarCuandoGameOver;
+
+    private static string KEY_HIGHSCORE = "HIGHSCORE"; 
 
     private void Awake()
     {
+        if (PlayerPrefs.HasKey(KEY_HIGHSCORE)){
+            puntuacionMaxima = PlayerPrefs.GetInt(KEY_HIGHSCORE);
+        } else {
+            puntuacionMaxima = 0;
+        }
+        textoPuntuacion.text=puntuacion.ToString();
+        textoPuntuacionMaxima.text=puntuacionMaxima.ToString();
         Instance = this;
         DontDestroyOnLoad(gameObject);
     }
 
     private void Start(){
         ActualizarBarraDeSalud();
+    }
+
+    private void Update(){
+        if (salud==0){
+            if (Input.GetKeyDown(KeyCode.Space)) 
+            {
+                ReiniciarJuego();
+            }
+        }
     }
 
     public void DecrementarSalud(int decrementoSalud)
@@ -46,7 +70,23 @@ public class GameManager : MonoBehaviour
         imageVida.fillAmount=salud/saludMaxima;
     }
     public void TerminarJuego(){
+        //1. Aparecen los textos de GameOver y de continuar y el resto de objetos que quieras
+        foreach (GameObject objeto in objetosAActivarCuandoGameOver){
+            objeto.SetActive(true);
+        }
+        //2. Guardar la puntuación si es puntuación máxima?
+        if (puntuacion>puntuacionMaxima){
+            PlayerPrefs.SetInt(KEY_HIGHSCORE, puntuacion);
+            PlayerPrefs.Save();
+        }
         SceneManager.LoadScene(3);
+    }
 
+    public void ReiniciarJuego(){
+        SceneManager.LoadScene(0);
+    }
+    public void Puntuar(int puntuacion){
+        this.puntuacion+=puntuacion;
+        this.textoPuntuacion.text=this.puntuacion.ToString();
     }
 }
--- Assets/_GameAssets/Scripts/UIScript.cs
+++ Assets/_GameAssets/Scripts/UIScript.cs
@@ -4,18 +4,26 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class UIScript : MonoBehaviour
-{ public AudioSource audioSource;
+{
+  public AudioSource audioSource;
   public AudioClip loopIntro;
   public AudioClip click;
-  public void Jugar(){
-    SceneManager.LoadScene(1);
- } 
-  public void Exit(){
-        Application.Quit();
 
-    }
-    public void PlayClick(){
-      audioSource.PlayOneShot(click);
-    }
- 
+  public GameObject Intro;
+
+  public void Jugar()
+  {
+
+    SceneManager.LoadScene(1);
+  }
+  public void Exit()
+  {
+    Application.Quit();
+
+  }
+  public void PlayClick()
+  {
+    audioSource.PlayOneShot(click);
+  }
+
 }
